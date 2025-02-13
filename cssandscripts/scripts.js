4e2cb3085ea3a26a3b5e094f6642c0724c41cbf8
Ravi Bhuvan <ravibhuvan31@gmail.com>
2025-02-14 02:01:28 +0530

    major update

--- cssandscripts/scripts.js
+++ cssandscripts/scripts.js
@@ -20,16 +20,16 @@ async function getWeather() {
 
 function displayWeather(data) {
   if (data && data.location && data.current) {
+    document.querySelector(".message").innerHTML = `Its actually ${data.current.condition.text}`;
     // Display weather data
     document.getElementById("weatherDisplay").innerHTML = `
       <div class="weather-box">
-        <h3>City Of ${data.location.name}, ${data.location.country}</h3>
+        <p>City Of ${data.location.name}, ${data.location.country}</p>
       </div>
       <div class="weather-box">
         <p>Temperature: ${data.current.temp_c}°C</p>
       </div>
       <div class="weather-box">
-        <p>Weather: ${data.current.condition.text}</p>
         <img src="https:${data.current.condition.icon}" alt="Weather icon">
       </div>
       <div class="weather-box">
@@ -42,43 +42,13 @@ function displayWeather(data) {
         <p>Wind Speed: ${data.current.wind_mph} mph</p>
       </div>
     `;
-
-    // Change background based on weather condition
-    changeBackground(data.current.condition.text);
   } else {
     document.getElementById("weatherDisplay").innerHTML = `<p>City not found.</p>`;
   }
   console.log("did you get it !!");
 }
 
-// Function to change the background based on weather condition
-function changeBackground(condition) {
-  const body = document.body;
 
-
-  const normalizedCondition = condition.trim().toLowerCase().replace(/\s+/g, '_'); // Normalize the condition
-
-  // background images
-  const weatherBackgrounds = {
-    "clear": "url('../images/clear.jpeg')",
-    "sunny": "url('../images/sunny.jpeg')",
-    "rain": "url('../images/rainy.jpeg')",
-    "partly_cloudy": "url('../images/clouds.jpeg')", 
-    "snow": "url('../images/snowy.jpeg')",
-    "thunderstorm": "url('../images/thunder.jpeg')",
-    "mist": "url('../images/mist.jpeg')",
-    "haze": "url('../images/haze.jpeg')",
-  };
-
-  // Default background if no match
-  const defaultBackground = "url('../images/back.jpeg')";
-
-  // Set the background based on condition or default
-  body.style.backgroundImage = weatherBackgrounds[normalizedCondition] || defaultBackground;
-  body.style.backgroundSize = "cover"; // Ensure the image covers the screen
-  body.style.backgroundRepeat = "no-repeat";
-  // body.style.transition = "background 0.5s ease";// Smooth transition 
-}
 document.addEventListener('keydown', function(event) {
             if (event.key === 'Enter') {
                 document.getElementById('getweather').click();
